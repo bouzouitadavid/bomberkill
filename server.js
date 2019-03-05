4feec7ea05670367ae138569110bbffb18a0ca8b
bouzouitadavid <davidbouzouita@gmail.com>
2019-03-05 12:57:07 +0100

    change directory

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(app);
 
 var players = {};
 
